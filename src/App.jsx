a45427dd12b9922ac89dadb864f253b74962c6ba
Harruta <haruubusinesss@gmail.com>
2025-08-19 12:50:08 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css"
+import { RequestAirdrop } from "./airdrop";
+
+function App() {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  return (
+    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/V2O-WWPQn9hNVj25_ON6z"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{ display: "flex", justifyContent: "space-between"}}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+            <RequestAirdrop />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default App;
